bb1c1d157c8459d4fc7b89daed324e76eb2582a1
TounDavid <vitrith74@gmail.com>
2025-03-18 21:30:25 +0700

    Add files via upload

---
+++ index.html.js
@@ -0,0 +1,36 @@
+<DOCTYPE html>
+<html>
+<head>
+<title> College CoST </title>
+<link rel="icon" type="image/x-icon" href="images/uc.png">
+</head>
+
+<body style="background-color:lightgray;">
+<h2 style="background-color:darkblue; color:white; font-family:kh Moul; text-align:center; border:3px solid #09E867; font-size:200%;">មហាវិទ្យាល័យវិទ្យាសាស្រ្តនិងបច្ចេកវិទ្យា</h2>
+<a href="https://cost.uc.edu.kh"> visit College science and Technology</a><br>
+
+<p style="color:darkblue; border:2px solid #09E867;">
+1. Information Technology<br>
+2. Computer science<br>
+3. Electronice and Telecommunication<br>
+4. Graphic Desige and Multimedia<br>
+5. Business Information Technology<br>
+6. Network Ingrneering and cybersecurity<br>
+7. Ai and data science
+</p>
+<p style="color:darkblue"> Dean's Message Information On behalf of faculty members and staff of the college of Science and Technology I wound like to wellcome you to the College of Science and Technology at The University Of Cambodia.The College Of Science and Technology (CoST) was established at the same time as the The University of Cambodia in 2013
+ </p>
+ <h2 style="background-color:darkblue; color:white; border:2px solid #09E867; font-size:150% font-family:Alfredo Heavy; text-align:center;"> បន្ទប់អនុវត្ត-Computer Labs </h2>
+ <img src="images/lab1.jpg" width="300" height="200">
+ <img src="images/lab2.jpg" width="300" height="200">
+ <img src="images/lab3.jpg" width="300" height="200"><br>
+ </body>
+ <footer>
+ <img src="images/icon.jpg" width="50" heilght="50">
+ College Science and Technology Coppyright@2024
+ </footer>
+ <p> Toun David
+ </p>
+ 
+ 
+</html>
